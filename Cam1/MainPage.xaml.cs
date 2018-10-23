4892429ad0511bd940af568d9f2de3c6d375e785
隼斗 小林 <wahuku.miyatuguti_35@hotmail.co.jp>
2018-10-23 22:30:28 +0900

    preview coding...

--- Cam1/MainPage.xaml.cs
+++ Cam1/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using Windows.Media;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
+using Windows.System.Threading;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
@@ -32,15 +33,116 @@ namespace Cam1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MediaCapture capture;
+        private bool isPreview = false;
+        private bool isRecording = false;
+        private ThreadPoolTimer timer;
+        private Timer recordingTimer;
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        public void ExecuteCapture()
+        public async Task InitCameraAsync()
         {
-            
+            try
+            {
+                if (capture == null)
+                {
+                    if (isPreview)
+                    {
+                        await capture.StopPreviewAsync();
+                        isPreview = false;
+                    }
+                    capture.Dispose();
+                    capture = null;
+                }
 
+                //カメラの設定
+                var captureInitSettings = new MediaCaptureInitializationSettings();
+                captureInitSettings.VideoDeviceId = "";
+                captureInitSettings.StreamingCaptureMode = StreamingCaptureMode.Video;
+
+                var camera = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+                if(camera.Count() == 0)
+                {
+                    Debug.WriteLine("No Camera");
+                    return;
+                }
+                else
+                {
+                    if(camera.Count() == 1)
+                    {
+                        captureInitSettings.VideoDeviceId = camera[0].Id;
+                    }
+                    else
+                    {
+                        captureInitSettings.VideoDeviceId = camera[1].Id;
+                    }
+                }
+
+                capture = new MediaCapture();
+                await capture.InitializeAsync(captureInitSettings);
+
+                //ビデオの設定
+                VideoEncodingProperties vp = new VideoEncodingProperties();
+                vp.Height = 240;
+                vp.Width = 320;
+                vp.Subtype = "NV12";
+
+                await capture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, vp);
+                preview.Source = capture;
+
+                Debug.WriteLine("Camera Initialized");
+
+                //プレビュー開始
+                await StartPreview();
+
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task StartPreview()
+        {
+            Debug.WriteLine("Start Preview");
+            await capture.StartPreviewAsync();
+            isPreview = true;
+
+            if(timer == null)
+            {
+                timer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(CurrentVideoFrame), TimeSpan.FromMilliseconds(66));
+            }
+        }
+
+        public async void CurrentVideoFrame(ThreadPoolTimer timer)
+        {
+            if (!semaphore.Wait(0))
+            {
+                return;
+            }
+
+            try
+            {
+                using(VideoFrame previewFrame = new VideoFrame(BitmapPixelFormat.Nv12, 320, 240))
+                {
+                    await capture.GetPreviewFrameAsync(previewFrame);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
         }
     }
 }
